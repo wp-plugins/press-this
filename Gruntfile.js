0fd0cc302a175a08f846676f55b70ff69a655ff1
githubsync <githubsync@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-30 01:31:17 +0000

    commit d0ee1fca4b7f78cf71544de3a3e7d9c344c6b539 Author: MichaelArestad <marestad@gmail.com> Date:   Thu Jan 29 18:26:35 2015 -0700
        First
    
    
    git-svn-id: https://plugins.svn.wordpress.org/press-this/trunk@1078650 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,138 +1,123 @@
 module.exports = function(grunt) {
 
-    // 1. All configuration goes here
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+// 1. All configuration goes here
+grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
 
-        // concat: {
-        //     dist: {
-        //         src: [
-        //             'js/main/*.js',
-        //         ],
-        //         dest: 'js/production.js', // needs a better name, doncha think?
-        //     }
-        // },
-        // uglify: {
-        //     build: {
-        //         src: 'js/production.js',
-        //         dest: 'js/production.min.js'
-        //     }
-        // },
-        sass: {
-            dist: {
-                options: {
-                    // Can be nested, compact, compressed, expanded
-                    style: 'expanded',
-                    sourcemap: true
-                },
-                files: {
-                    'css/press-this.css': 'scss/style.scss'
-                }
-            }
-        },
-        autoprefixer: {
-            options: {
-                // Task-specific options go here.
-            },
-            global: {
-                options: {
-                    // Target-specific options go here.
-                    // browser-specific info: https://github.com/ai/autoprefixer#browsers
-                    // DEFAULT: browsers: ['> 1%', 'last 2 versions', 'ff 17', 'opera 12.1']
-                    browsers: ['> 1%', 'last 2 versions', 'ff 17', 'opera 12.1', 'ie 8', 'ie 9']
-                },
-                src: 'css/press-this.css'
-            },
-        },
-        grunticon: {
-            dashicons: {
-                files: [{
-                    expand: true,
-                    cwd: 'images/icons/',
-                    src: [
-                        '*.svg',
-                        '*.png',
-                        '!icons.svg'
-                    ],
-                    dest: "images/icons/"
-                }],
-                options: {
-                    defaultWidth: '20px',
-                    defaultHeight: '20px'
-                }
-            }
-        },
-        svgmin: {
-            options: {
-                plugins: [
-                    { removeViewBox: false },
-                    { removeUselessStrokeAndFill: false }
-                ]
-            },
-            dist: {
-                files: [{               // Dictionary of files
-                    expand: true,       // Enable dynamic expansion.
-                    cwd: 'images/icons',     // Src matches are relative to this path.
-                    src: ['*.svg', '!icons.svg'],  // Actual pattern(s) to match.
-                    dest: 'images/icons',       // Destination path prefix.
-                    ext: '.svg'     // Dest filepaths will have this extension.
-                    // ie: optimise img/src/branding/logo.svg and store it in img/branding/logo.min.svg
-                }]
-            }
-        },
-        svgstore: {
-            defaults: {
-                options: {
-                    prefix : 'dashicons-', // This will prefix each ID
-                    svg: { // will be added as attributes to the resulting SVG
-                        viewBox : '0 0 20 20',
-                        class : 'icon-defs'
-                    }
-                },
-                files: {
-                    'images/icons/icons.svg': ['images/icons/*.svg', '!images/icons/icons.svg']
-                },
-            }
-        },
-        watch: {
-            // options: {
-            //     livereload: true,
-            // },
-            // scripts: {
-            //     files: ['js/*.js'],
-            //     tasks: ['concat', 'uglify'],
-            //     options: {
-            //         spawn: false,
-            //     },
-            // },
-            css: {
-                files: ['scss/*.scss', 'scss/**/*.scss'],
-                tasks: ['sass', 'autoprefixer'],
-                options: {
-                    spawn: false,
-                }
-            },
-            svg: {
-                files: ['images/icons/*.svg'],
-                tasks: ['svgmin', 'svgstore', 'grunticon'],
-                options: {
-                    spawn: false,
-                }
-            }
+  sass: {
+    dist: {
+      options: {
+        // Can be nested, compact, compressed, expanded
+        style: 'expanded'
+      },
+      files: {
+        'css/press-this.css': 'scss/style.scss'
+      }
+    }
+  },
+
+  autoprefixer: {
+    options: {
+      // Task-specific options go here.
+    },
+    global: {
+      options: {
+        // Target-specific options go here.
+        // browser-specific info: https://github.com/ai/autoprefixer#browsers
+        // DEFAULT: browsers: ['> 1%', 'last 2 versions', 'ff 17', 'opera 12.1']
+        browsers: ['> 1%', 'last 2 versions', 'ff 17', 'opera 12.1', 'ie 8', 'ie 9']
+      },
+      src: 'css/press-this.css'
+    },
+  },
+
+  grunticon: {
+    dashicons: {
+      files: [{
+        expand: true,
+        cwd: 'images/icons/.tmp-fallbacks/',
+        src: [ '*.svg' ],
+        dest: "images/icons/"
+      }],
+      options: {
+        defaultWidth: '20px',
+        defaultHeight: '20px',
+        colors: {
+          white: "#ffffff"
         }
-    });
+      }
+    }
+  },
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-concat');     // concatenate
-    grunt.loadNpmTasks('grunt-contrib-uglify');     // minify
-    grunt.loadNpmTasks('grunt-contrib-watch');      // watch files for changes
-    grunt.loadNpmTasks('grunt-contrib-sass');       // Gettin Sassy!
-    grunt.loadNpmTasks('grunt-autoprefixer');       // Auto-freaking-prefixer!!!
-    grunt.loadNpmTasks('grunt-grunticon');          // Grunticon!
-    grunt.loadNpmTasks( 'grunt-svgmin' );           // SVG minifier
-    grunt.loadNpmTasks('grunt-svgstore');           // SVG combiner
+  svgmin: {
+    options: {
+      plugins: [
+        { removeViewBox: false },
+        { removeUselessStrokeAndFill: false }
+      ]
+    },
+    dist: {
+      files: [{
+        expand: true,
+        cwd: 'images/icons/src/',
+        src: [ '*.svg' ],
+        dest: 'images/icons/.tmp',
+        ext: '.svg'
+      }]
+    },
+    fallbacks: {
+      files: [{
+        expand: true,
+        cwd: 'images/icons/src/',
+        src: [ '*.svg' ],
+        dest: 'images/icons/.tmp-fallbacks',
+        ext: '.colors-white.svg'
+      }]
+    }
+  },
 
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['watch']);
+  svgstore: {
+    defaults: {
+      options: {
+        prefix : 'dashicons-',
+        svg: {
+          viewBox : '0 0 20 20',
+          class : 'dashicons-bundle'
+        }
+      },
+      files: {
+        'images/icons/dashicons.svg': [ 'images/icons/.tmp/*.svg' ]
+      },
+    }
+  },
+
+  watch: {
+    css: {
+      files: ['scss/*.scss', 'scss/**/*.scss'],
+      tasks: ['sass', 'autoprefixer'],
+      options: {
+        spawn: false,
+      }
+    },
+    svg: {
+      files: ['images/icons/*.svg'],
+      tasks: ['svgmin', 'svgstore', 'grunticon'],
+      options: {
+        spawn: false,
+      }
+    }
+  }
+});
+
+// 3. Where we tell Grunt we plan to use this plug-in.
+grunt.loadNpmTasks('grunt-contrib-watch');      // watch files for changes
+grunt.loadNpmTasks('grunt-contrib-sass');       // Gettin Sassy!
+grunt.loadNpmTasks('grunt-autoprefixer');       // Auto-freaking-prefixer!!!
+grunt.loadNpmTasks('grunt-grunticon');          // Grunticon!
+grunt.loadNpmTasks( 'grunt-svgmin' );           // SVG minifier
+grunt.loadNpmTasks('grunt-svgstore');           // SVG combiner
+
+// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+grunt.registerTask('default', ['watch']);
 
 };
